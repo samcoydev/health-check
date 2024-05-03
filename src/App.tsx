0d79571addfa3ac95a4d80ffa72d09a646c8084a
samcoydev <samuelcoydev@gmail.com>
2024-05-03 12:40:15 -0500

    Fixed light mode not working

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,30 @@ import StatusEntry from "./components/StatusEntry";
 import apps from "./apps-list";
 
 function App() {
-    const activeApps = apps.filter(a => !a.inProgress);
-    const inProgressApps = apps.filter(a => a.inProgress);
+  const activeApps = apps.filter((a) => !a.inProgress);
+  const inProgressApps = apps.filter((a) => a.inProgress);
 
-    return (
-        <div className="mx-auto max-w-lg px-4 flex flex-col content-start">
-            <div className="mb-5">
-                <h1 className="text-4xl">samcodesthings.</h1>
-                <h3 className="text-lg">health check app</h3>
-            </div>
-            <div>
-                {activeApps.map((status, index) => (
-                    <StatusEntry status={status} key={index} />
-                ))}
-            </div>
-            <div className="my-5">
-                <h5 className="ml-1 text-start">Inactive Apps</h5>
-                <div className="py-3 my-1 border-t-2 border-solid">
-                    {inProgressApps.map((status, index) => (
-                        <StatusEntry status={status} key={index} />
-                    ))}
-                </div>
-            </div>
+  return (
+    <div className="mx-auto max-w-lg px-4 flex flex-col content-start">
+      <div className="mb-5">
+        <h1 className="text-4xl">samcodesthings.</h1>
+        <h3 className="text-lg">health check app</h3>
+      </div>
+      <div>
+        {activeApps.map((status, index) => (
+          <StatusEntry status={status} key={index} />
+        ))}
+      </div>
+      <div className="my-5">
+        <h5 className="ml-1 text-start">Inactive Apps</h5>
+        <div className="py-3 my-1 border-t-2 border-solid">
+          {inProgressApps.map((status, index) => (
+            <StatusEntry status={status} key={index} />
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default App;
